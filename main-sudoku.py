c280b213ba9c30247168ceb89d91b42f8c2becdc
tartempion-sdr <93546844+tartempion-sdr@users.noreply.github.com>
2024-06-01 16:41:07 +0200

    display

--- main-sudoku.py
+++ main-sudoku.py
@@ -1,1 +1,18 @@
 #main-sudoku.py
+
+import pygame 
+import sys
+
+pygame.init()
+pygame.display.set_mode((400,400))
+pygame.display.set_caption("chromino")
+
+
+game_on = True
+
+while game_on:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
