1432f32e2e0465444f2cfbb657b7718ff2b947f7
tartempion-sdr <93546844+tartempion-sdr@users.noreply.github.com>
2024-06-01 17:24:56 +0200

    display-background-purple

--- main-sudoku.py
+++ main-sudoku.py
@@ -4,8 +4,13 @@ import pygame
 import sys
 
 pygame.init()
-pygame.display.set_mode((400,400))
+screen = pygame.display.set_mode((400,400))
+clock = pygame.time.Clock()
 pygame.display.set_caption("chromino")
+
+
+
+
 
 
 game_on = True
@@ -13,6 +18,12 @@ game_on = True
 while game_on:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            game_on = False
             pygame.quit()
             sys.exit()
     pygame.display.update()
+    screen.fill((128,0,128))
+    pygame.display.flip()
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
